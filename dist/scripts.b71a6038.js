408a132df150aec34327932bb4e76ca57d08df31
Denny Tek <tek.denny@gmail.com>
2021-09-23 11:54:46 -0400

    Build for gh-pages

--- dist/scripts.b71a6038.js
+++ dist/scripts.b71a6038.js
@@ -22647,7 +22647,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62605" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51738" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
